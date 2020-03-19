b22d6af737e4194754a8ea6dfcd4317fc8cfe448
Christian Hanchett <cahanche@umich.edu>
2020-03-19 18:15:48 -0400

    Class functions set up and ready to be defined

--- Fraction/Fraction.py
+++ Fraction/Fraction.py
@@ -1,1 +1,18 @@
+import math
 
+class Fraction:
+    def __init__(self, numerator = 0, denominator = 1):
+        self.numerator = numerator
+        self.denominator = denominator
+
+    def _common_denominator(self, other):
+
+    def _simplify(self):
+
+    def __add__(self, other):
+
+    def __sub__(self, other):
+
+    def __mul__(self, other):
+
+    def __truediv__(self, other):
